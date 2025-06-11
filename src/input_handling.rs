5b4790574b567a709825a0be835eba48d1faf174
darrkenn <darrkenn08@gmail.com>
2025-06-11 20:07:14 +0100

    uhoh idk if this works on linux

--- src/input_handling.rs
+++ src/input_handling.rs
@@ -1,22 +1,56 @@
-pub fn handle_input(input: Vec<&str>) {
-
+use std::{env, fs, io};
+use std::path::PathBuf;
+pub fn handle_input(input: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let default_path = env::current_dir()?;
+    let length = input.len();
     match input.as_slice() {
-
         /////////////////////
         ////    Cat      ////
         /////////////////////
-        ["cat", args @ ..] if !args.is_empty() => {
-            for word in args {
-                println!("{}", word);
-            }
+        ["cat", args @ ..] if !args.is_empty() && length > 2 => {
+            let string: Vec<&str> = <[&str]>::into_vec(Box::from(args));
+            println!("{}", string.join(" "));
+        }
+        ["cat", arg] if !arg.is_empty() && length == 2 => {
+            println!("{}", arg);
         }
         ["cat"] => {
-            println!("provide text");
+            println!("Provide text");
         }
-
         /////////////////////
         ////    Clear    ////
         /////////////////////
+        ["clear"] => {
+            clear_screen::clear();
+        }
+        /////////////////////
+        ////    Dir      ////
+        /////////////////////
+        ["dir"] => {
+            println!("{}", default_path.display());
+            for file in fs::read_dir(default_path).unwrap() {
+                println!("{}", file.unwrap().path().display());
+            }
+        }
+        /////////////////////
+        ////      Ls     ////
+        /////////////////////
+        ["ls"] => {
+            let entries = handle_ls(default_path)?;
+            for entry in entries.iter() {
+                println!("{}", entry);
+            }
+        }
+        ["ls", arg] if !arg.is_empty() && length == 2 => {
+            let entries = handle_ls(arg.parse().unwrap())?;
+            for entry in entries.iter() {
+                println!("{}", entry);
+            }
+        }
+        
+        ["cd", arg] if !arg.is_empty() && length == 2 => {
+            env::set_current_dir(arg)?;
+        }
 
 
 
@@ -24,4 +58,36 @@ pub fn handle_input(input: Vec<&str>) {
             println!("Invalid");
         }
     }
+    Ok(())
+}
+
+
+fn handle_ls(path: PathBuf) -> io::Result<Vec<String>> {
+    let metadata = fs::metadata(&path)?;
+
+    if metadata.is_file() {
+        if let Some(file_name) = path.file_name() {
+            if let Some(name) = file_name.to_str() {
+                return Ok(vec![name.to_owned()]);
+            }
+        }
+        return Ok(vec![]);
+    }
+
+    if !metadata.is_dir() {
+        return Ok(vec![]);
+    }
+
+    let mut files = Vec::new();
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        if let Some(name) = entry.file_name().to_str() {
+            if entry.metadata()?.is_dir() {
+                files.push(format!("{}/", name));
+            } else {
+                files.push(name.to_string());
+            }
+        }
+    }
+    Ok(files)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod input_handling;
 
-use std::io;
+use std::{env, io};
+use std::io::{stdout, Write};
+use std::path::Path;
 use crate::input_handling::handle_input;
 
 fn main() {
+    let root = Path::new("/");
+    env::set_current_dir(&root).is_ok();
     println!("Welcome to DarkShell!");
     loop {
         let mut input = String::new();
+        print!(">> ");
+        stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
         let parsed: Vec<&str> = input.trim().split(' ').collect();
 
