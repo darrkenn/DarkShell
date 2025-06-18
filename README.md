bb80587be36f8af6c326fd33ab6ebbc3337b15f3
darragh <darrkenn08@gmail.com>
2025-06-18 19:33:09 +0000

    Readme and username handling on line prompt.

---
+++ README.md
@@ -0,0 +1,6 @@
+# DarkShell
+DarkShell is a custom shell built in rust.
+
+IDK how to write README's.
+
+**Only tested on linux**
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 mod input_handling;
-use std::{env, fs, io};
+use std::{env,io};
 use std::env::home_dir;
 use std::io::{stdout, Write};
 use ansi_term::Color::{Blue, Green};
-use serde::{Deserialize, Serialize};
+use whoami::username;
 use crate::input_handling::handle_input;
 fn main() {
     let home = home_dir().unwrap();
     env::set_current_dir(&home).expect("Uhoh cant set dir to home.");
     println!("Welcome to DarkShell!");
+    let username = username().unwrap().to_string();
     loop {
-        
         let mut input = String::new();
-        handle_current_dir();
-        // print!("{:?}", current_dir);
-        // print!("{}", format!("{:?}{}", current_dir,Blue.paint(">> "))) ;
+        handle_current_dir(username.clone());
         stdout().flush().unwrap();
         input.clear();
         io::stdin().read_line(&mut input).unwrap();
@@ -22,14 +20,14 @@ fn main() {
         handle_input(parsed, home.clone()).expect("Cant process input");
     }
 }
-
-fn handle_current_dir() {
+fn handle_current_dir(username: String) {
     match env::current_dir() {
         Ok(path) => {
             if let Some(dir_name) = path.file_name() {
-                print!("{}", format!("{}{}", Green.paint( dir_name.to_string_lossy()),Blue.paint(">> ")));
+                print!("{}", format!("{}@{}{}",Green.paint(username), Green.paint( dir_name.to_string_lossy()),Blue.paint(">> ")));
             } else {
-                print!("{}", format!("{}{}", Green.paint("/"),Blue.paint(">> "))) ;
+                //Handles root directory better somehow idk.
+                print!("{}", format!("{}{}", Green.paint("/"),Blue.paint(">> ")));
             }
         }
         Err(err) => {println!("{}", err)}
