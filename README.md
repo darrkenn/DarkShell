95b00ec5b91bd5bc0a68da39024e8c93c79b17c3
darragh <darrkenn08@gmail.com>
2025-06-19 11:37:00 +0000

    Pipe spliting and detection

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DarkShell
+![](DarkShell.png)
 DarkShell is a custom shell built in rust.
 
 IDK how to write README's.
--- src/input_handling.rs
+++ src/input_handling.rs
@@ -5,19 +5,51 @@ mod process_commands;
 use std::{env};
 use std::io::stdout;
 use std::path::{PathBuf};
+use std::process::{Command, Stdio};
 use console::{Key, Term};
+use log::error;
 use crate::input_handling::misc_commands::{handle_clear, handle_exit, handle_help, handle_external_command, };
 use crate::input_handling::directory_commands::{handle_cd, handle_current_ls, handle_pwd,};
 use crate::input_handling::process_commands::{handle_getorkill_ps, handle_single_ps};
 use crate::input_handling::text_commands::{handle_say};
 
+
+fn handle_command_test(command: &str, args: &[&str]) {
+    let output = Command::new(command.to_string())
+        .args(args)
+        .env("TERM", "xterm-256color")
+        .stdout(Stdio::piped())
+        .output();
+    match output {
+        Ok(output) => {
+            let stdout = String::from_utf8(output.stdout).expect("Whoops");
+        }
+        Err(_error) => {
+            // println!("{}", _error);
+        }
+    }
+}
+
 pub fn handle_input(input: Vec<&str>, home: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    if input.contains(&"|")  {
+        if input.first() == Some(&"|") || input.last() == Some(&"|") {
+            println!("Cant put pipe at start or finish")
+        }
+        //This doesn't feel right, but it works
+        let joined_input = input.join(" ");
+        let commands = joined_input.split("|").map(str::trim).filter(|x| !x.is_empty()).collect::<Vec<&str>>();
+        for command in commands {
+            let vector: Vec<&str> = command.split_whitespace().collect();
+            let command = vector[0];
+            let args = vector[1..].to_vec();
+            println!("Seperated command via pipe");
+            handle_command_test(command, &args);
+        }
+
+    }
     let current_path = env::current_dir()?;
     let length = input.len();
     let sliced_input = input.as_slice();
-    if sliced_input[length - 1] == "*"  {
-
-    }
 
     match sliced_input {
         ["say", args @ ..] if !args.is_empty() => handle_say(args, length),
@@ -32,7 +64,7 @@ pub fn handle_input(input: Vec<&str>, home: PathBuf) -> Result<(), Box<dyn std::
         
         ["ps", arg] if !arg.is_empty() => handle_single_ps(arg),
         ["pid", args @ ..] if !args.is_empty() && length == 3 => handle_getorkill_ps(args),
-        
+
         _ => {
             let command = input[0];
             let args = &input[1..];
