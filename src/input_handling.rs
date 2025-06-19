19835a860386ecc182001a856ad2c8e11fb2a54f
darragh <darrkenn08@gmail.com>
2025-06-19 08:55:43 +0000

    Basic command history accessed by using arrow key up and down.

--- src/input_handling.rs
+++ src/input_handling.rs
@@ -2,9 +2,10 @@ mod misc_commands;
 mod text_commands;
 mod directory_commands;
 mod process_commands;
-
 use std::{env};
+use std::io::stdout;
 use std::path::{PathBuf};
+use console::{Key, Term};
 use crate::input_handling::misc_commands::{handle_clear, handle_exit, handle_help, handle_external_command, };
 use crate::input_handling::directory_commands::{handle_cd, handle_current_ls, handle_pwd,};
 use crate::input_handling::process_commands::{handle_getorkill_ps, handle_single_ps};
@@ -37,6 +38,76 @@ pub fn handle_input(input: Vec<&str>, home: PathBuf) -> Result<(), Box<dyn std::
             let args = &input[1..];
             handle_external_command(command, args);
             Ok(())
+        }
+    }
+}
+
+pub fn handle_command_input(past_commands: &mut Vec<String>) -> String {
+    let term = Term::stdout();
+    let mut chars: Vec<char> = Vec::new();
+    let mut iter: u8 = 0;
+    let vec_length = past_commands.len();
+    loop {
+        let key = term.read_key().unwrap();
+        match key {
+            Key::Char(c) => {
+                chars.push(c);
+                print!("{c}");
+                std::io::Write::flush(&mut stdout()).unwrap();
+            }
+            Key::ArrowUp =>{
+                //Im sure theres a much better way to do this but its working and im not complaining
+                if past_commands.is_empty() {continue}
+                if vec_length == iter as usize{continue}
+                if vec_length >= 1 {
+                    for _c in chars.iter() {
+                        print!("\x08 \x08");
+                    }
+                    iter+=1;
+                    let command = &past_commands[vec_length-iter as usize];
+                    chars.clear();
+                    for  c in command.chars() {
+                        chars.push(c);
+                        print!("{c}");
+                    }
+                    std::io::Write::flush(&mut stdout()).unwrap();
+                }
+            }
+            Key::ArrowDown => {
+                //Yeah this is shady
+                if past_commands.is_empty() {continue}
+                if vec_length >= 1 {
+                    if iter != 0 {
+                        for _c in chars.iter() {
+                            print!("\x08 \x08");
+                        }
+                        let command = &past_commands[vec_length-iter as usize];
+                        iter-=1;
+                        chars.clear();
+                        for  c in command.chars() {
+                            chars.push(c);
+                            print!("{c}");
+                        }
+                        std::io::Write::flush(&mut stdout()).unwrap();
+                    }
+                }
+            }
+            Key::Backspace => {
+                if !chars.is_empty() {
+                    chars.pop();
+                    print!("\x08 \x08");
+                    std::io::Write::flush(&mut stdout()).unwrap();
+                }
+            }
+            Key::Enter => {
+                let input = chars.iter().collect::<String>();
+                if input.is_empty() {continue}
+                println!();
+                return input
+            }
+            _ => {
+
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 mod input_handling;
-use std::{env,io};
+use std::{env};
 use std::env::home_dir;
 use std::io::{stdout, Write};
-use std::str::Chars;
 use ansi_term::Color::{Blue, Green};
 use whoami::username;
-use crate::input_handling::handle_input;
-use console::{Key, Term};
+use crate::input_handling::{handle_command_input, handle_input};
 fn main() {
     let home = home_dir().unwrap();
     env::set_current_dir(&home).expect("Uhoh cant set dir to home.");
     println!("Welcome to DarkShell!");
     let username = username().unwrap().to_string();
     let mut past_commands: Vec<String> = Vec::new();
-
     loop {
         handle_current_dir(username.clone());
         stdout().flush().unwrap();
-        let input = handle_user_input(&mut past_commands);
+        let input = handle_command_input(&mut past_commands);
         past_commands.push(input.clone());
         let parsed: Vec<&str> = input.trim().split(' ').collect();
         handle_input(parsed, home.clone()).expect("Cant process input");
@@ -36,72 +33,7 @@ fn handle_current_dir(username: String) {
         Err(err) => {println!("{}", err)}
     }
 }
-fn handle_user_input(past_commands: &mut Vec<String>) -> String {
-    let term = Term::stdout();
-    let mut chars: Vec<char> = Vec::new();
-    let mut iter: u8 = 0;
-    loop {
-        let key = term.read_key().unwrap();
-        match key {
-            Key::Char(c) => {
-                chars.push(c);
-                print!("{c}");
-                std::io::Write::flush(&mut stdout()).unwrap();
-            }
-            Key::ArrowUp =>{
-                iter+=1;
-                if past_commands.len() > iter as usize -1  {
-                    let command = &past_commands[iter as usize - 1];
 
-                    print!("{command}");
-                    std::io::Write::flush(&mut stdout()).unwrap();
-                } else {
-                    print!("test");
-                    std::io::Write::flush(&mut stdout()).unwrap();
-                }
-            }
-            Key::ArrowDown => {
-                if iter != 0 {
-                    iter -= 1;
-                    if past_commands.len() < iter as usize -1  {
-                        let command = &past_commands[iter as usize - 1];
-
-                        print!("{command}");
-                        std::io::Write::flush(&mut stdout()).unwrap();
-                    } else {
-                        print!("test");
-                        std::io::Write::flush(&mut stdout()).unwrap();
-                    }
-                } else {
-                    if past_commands.len() != 0 {
-                        let command = &past_commands[0];
-                        print!("{command}");
-                        std::io::Write::flush(&mut stdout()).unwrap();
-                    } else {
-                        print!("is nothing");
-                        std::io::Write::flush(&mut stdout()).unwrap();
-                    }
-                }
-            }
-            Key::Backspace => {
-                if !chars.is_empty() {
-                    chars.pop();
-                    print!("\x08 \x08");
-                    std::io::Write::flush(&mut stdout()).unwrap();
-                }
-            }
-            Key::Enter => {
-                let input = chars.iter().collect::<String>();
-                println!();
-                return input
-            }
-            _ => {
-
-            }
-    }
-}
-    // io::stdin().read_line(&mut input).unwrap();
-}
 
 
 
